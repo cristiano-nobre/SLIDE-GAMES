19bd51c1743557702b79e8c2609b47ea4ee63ebe
Cristiano Nobre <cristianobremaia@gmail.com>
2022-09-29 19:52:50 -0300

    slide-games

---
+++ assets/js/slides.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const slideWrapper = document.querySelector('[data-slide="wrapper"]')
+const slideList = documemt.querySelector('[data-slide="list"]')
+const navPreviousButton = documemt.querySelector('[data-slide="nav-previous-button"]')
+const navNextButton = documemt.querySelector('[data-slide="nav-next-button"]')
+const controlsWrapper = documemt.querySelector('[data-slide="controls-wrapper"]')
+const slideItems = documemt.querySelectorAll('[data-slide="item"]')
+const controlsButtons = documemt.querySelectorAll('[data-slide="control-button"]')
+
+console.log(slideWrapper)
+console.log(slideList)
+console.log(navPreviousButton)
+console.log(navNextButton)
+console.log(controlsWrapper)
+console.log(slideItems)
+console.log(controlsButtons)
+
